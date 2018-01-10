7739af76ccaa280d264acdf7b8460e3c77e44beb
Kenny <865807345@qq.com>
2018-01-10 09:46:58 +0800

    完成 feign 测试

--- system-root/system-feign/src/main/java/com/system/aym/test/controller/SysUserInfoController.java
+++ system-root/system-feign/src/main/java/com/system/aym/test/controller/SysUserInfoController.java
@@ -2,9 +2,7 @@ package com.system.aym.test.controller;
 
 import com.system.aym.common.bean.ResponseData;
 import com.system.aym.test.feign.SysUserInfoFeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
--- system-root/system-feign/src/main/java/com/system/aym/test/feign/SysUserInfoFeignClient.java
+++ system-root/system-feign/src/main/java/com/system/aym/test/feign/SysUserInfoFeignClient.java
@@ -1,19 +1,22 @@
 package com.system.aym.test.feign;
 
-import com.system.aym.common.bean.ResponseData;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @FeignClient("TEST-AYM-API")
 public interface SysUserInfoFeignClient {
 
     @PostMapping(value = "/into")
-    String intoUserInfo(@PathVariable("userPhone") String userPhone, @PathVariable("userPwd")String userPwd);
+    String intoUserInfo(@RequestParam("userPhone") String userPhone, @RequestParam("userPwd") String userPwd);
 
     @PostMapping(value = "/findPhone")
-    String findByUserPhoneForUserInfo(@PathVariable("userPhone") String userPhone);
+    @ResponseBody
+    String findByUserPhoneForUserInfo(@RequestParam("userPhone") String userPhone);
 
     @PostMapping(value = "/findCode")
-    String findByUserCodeForUserInfo(@PathVariable("userCode") String userCode);
+    @ResponseBody
+    String findByUserCodeForUserInfo(@RequestParam("userCode") String userCode);
 }
